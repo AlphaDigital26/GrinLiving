4a61a05c781c2ed47fd6b1e4a6831f83c1b04c72
Ankita <210303105155@paruluniversity.ac.in>
2026-07-25 12:18:46 +0530

    added pagination

---
+++ animations.js
@@ -0,0 +1,5 @@
+// This file is currently a placeholder to prevent 404 errors.
+// Animations logic will be added here in the future.
+document.addEventListener('DOMContentLoaded', () => {
+    // Initial animation setup can go here
+});
